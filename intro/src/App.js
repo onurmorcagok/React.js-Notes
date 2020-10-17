f9a99e23a0cdb4b0e572a550e472e37455089565
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-10-17 16:35:39 +0300

    Notes added

---
+++ intro/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "./Navbar";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+
+import { Container, Row, Col } from "reactstrap";
+
+function App() {
+  let productInfo = { title: "ProductList" };
+  let categoryInfo = { title: "CategoryList" };
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Navbar />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={productInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={categoryInfo} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ intro/src/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Navbar Component</h2>
+      </div>
+    );
+  }
+}
