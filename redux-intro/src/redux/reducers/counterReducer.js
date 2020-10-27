ab6e2e348c32f425370d7d3423cdcadb7ae7924f
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-10-27 18:25:42 +0300

    Notes added

---
+++ redux-intro/src/redux/reducers/counterReducer.js
@@ -0,0 +1,18 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const counterReducer = (state = 10, action) => {
+  let newState;
+  switch (action.type) {
+    case actionTypes.INCREASE_COUNTER:
+      return (newState = state + action.payload);
+    case actionTypes.DECREASE_COUNTER:
+      return (newState = state - action.payload);
+    case actionTypes.INCREASE_BY_TWO_COUNTER:
+      // eslint-disable-next-line no-unused-vars
+      return (newState = state + action.payload);
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
