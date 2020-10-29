2dcef7a9591ffcf7da09561684939d888b33e7d8
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-10-29 16:04:59 +0300

    Notes added

---
+++ northwind-redux/src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import changeCategoryReducer from '../reducers/changeCategoryReducer'
+
+const rootReducer = combineReducers({
+    changeCategoryReducer
+})
+
+export default rootReducer;
