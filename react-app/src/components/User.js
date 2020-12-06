bd2202b90ab5ea07458cc1cae4b982c52c29e0e6
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-12-06 20:44:54 +0300

    Notes added

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -2,20 +2,31 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class User extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: false,
+    };
+  }
   render() {
+    // Destructing
     const { name, age, department } = this.props;
+    const { isVisible } = this.state;
     return (
       <div>
         <div className="col-md-8 mb-4">
           <div className="card">
             <div className="card-header d-flex justify-content-between">
               <h4 className="d-inline">{name} </h4>{" "}
-              <i className="fas fa-trash-alt" style={{cursor : "pointer"}}></i>
+              <i className="fas fa-trash-alt" style={{ cursor: "pointer" }}></i>
             </div>
-            <div className="card-body">
-              <p className="card-text">Age: {age}</p>
-              <p className="card-text">Department: {department}</p>
-            </div>
+            {isVisible ? (
+              <div className="card-body">
+                <p className="card-text">Age: {age}</p>
+                <p className="card-text">Department: {department}</p>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
