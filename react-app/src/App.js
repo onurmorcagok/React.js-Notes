531e0f216f0ab1b46f0f94adfbc63cfad71e3142
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-12-31 21:46:41 +0300

    Notes added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Users from "./components/Users";
 import AddUser from "./components/AddUser";
+import NotFound from "./components/NotFound";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Navbar title="User App" />
-        <hr />
-        <AddUser />
-        <Users />
-      </div>
+      <Router>
+        <div className="container">
+          <Navbar title="User App" />
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Users} />
+            <Route exact path="/addUser" component={AddUser} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Navbar(props) {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-        </div>
-    )
+function Navbar({ title }) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3 mp-3">
+      <a href="/" className="navbar-brand">
+        {title}
+      </a>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item active">
+          <Link to="/addUser" className="nav-link">
+            Add User
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
