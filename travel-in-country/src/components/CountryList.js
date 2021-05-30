0b522e7c0fd7235cf2cd979f47fcc5ebd9d9fd61
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-05-30 20:28:58 +0300

    add url's edited

--- travel-in-country/src/components/CountryList.js
+++ travel-in-country/src/components/CountryList.js
@@ -56,7 +56,11 @@ const CountryList = (props) => {
               </h6>
               <hr />
               <div className="d-flex justify-content-between align-items-center">
-                <Link to="/add" type="button" className="btn btn-md btn-info">
+                <Link
+                  to={`/add/${country.name}`}
+                  type="button"
+                  className="btn btn-md btn-info"
+                >
                   <i className="fas fa-comment"></i>
                 </Link>
                 <Link
