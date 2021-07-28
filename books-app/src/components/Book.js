622fab847bd32314baf2094deeadc94d0fcf9376
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-07-29 01:29:29 +0300

    Css and styles edited

--- books-app/src/components/Book.js
+++ books-app/src/components/Book.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 
+import "./Book.css";
+
 class Book extends Component {
   render() {
     return (
-      <div>
-        <h2>{this.props.book.title}</h2>
-        <h4> {this.props.book.author}</h4>
+      <div className="col-lg-4 col-sm-6 mb-4">
+        <div className="portfolio-item">
+          <a
+            className="portfolio-link"
+            data-toggle="modal"
+            href="#portfolioModal1"
+          >
+            <div className="portfolio-hover">
+              <div className="portfolio-hover-content">
+                <i className="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img
+              className="img-fluid"
+              src={this.props.book.imageURL}
+              alt=""
+            ></img>
+          </a>
+          <div className="portfolio-caption">
+            <div className="portfolio-caption-heading">
+              {this.props.book.title}
+            </div>
+            <div className="portfolio-caption-subheading">
+              <b>{this.props.book.author}</b>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- books-app/src/components/BookList.js
+++ books-app/src/components/BookList.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import Book from "./Book";
 
+import "./BookList.css";
 class BookList extends Component {
   render() {
     const bookList = this.props.books.map((book, i) => {
       return <Book book={book} key={i} />;
     });
 
-    return <div>{bookList}</div>;
+    return (
+      <section className="page-section bg-light" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">Books App</h2>
+            <h3 className="section-subheading text-muted">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit! Tenetur,
+              quo?
+            </h3>
+          </div>
+          <div className="row">{bookList}</div>
+        </div>
+      </section>
+    );
   }
 }
 
