5602ccbe7cd0db404986367a0019a5094bc29b71
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-07-28 18:42:27 +0300

    First commit

---
+++ books-app/src/components/Book.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.book.title}</h2>
+        <h4> {this.props.book.author}</h4>
+      </div>
+    );
+  }
+}
+
+export default Book;
---
+++ books-app/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class BookList extends Component {
+  render() {
+    const bookList = this.props.books.map((book, i) => {
+      return <Book book={book} key={i} />;
+    });
+
+    return <div>{bookList}</div>;
+  }
+}
+
+export default BookList;
